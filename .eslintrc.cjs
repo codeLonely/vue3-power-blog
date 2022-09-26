50fb462cf20e6b7c1feac4f66bcc03a7dca85764
hemiaoyang <hemiaoyang@abtnetworks.com>
2022-09-26 21:59:42 +0800

    feat(开发规范): 增加eslint和prettier代码风格质量规范

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,66 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // 组件属性顺序
+    'vue/attributes-order': 'warn',
+    // 注释开头和结尾在同一行
+    'vue/html-comment-content-newline': 'warn',
+    // 注释开头和结尾有空格
+    'vue/html-comment-content-spacing': 'warn',
+    'vue/multi-word-component-names': 'off',
+    // component name要和文件名匹配
+    'vue/match-component-file-name': [
+      'error',
+      {
+        extensions: ['js', 'jsx', '.ts', '.tsx', 'vue'], // 检测的拓展名
+        shouldMatchCase: true, // 需要匹配大小写
+      },
+    ],
+    // 禁止空template、script、style
+    'vue/no-empty-component-block': 'warn',
+    // 禁止不必要的插值表达式，如{{ 3 }}，可直接用3代替
+    'vue/no-useless-mustaches': 'warn',
+
+    // console、debugger
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 'off',
+
+    'vue/multiline-html-element-content-newline': 'warn',
+    // 关闭单html标签，内容需换行，因为其和prettier冲突
+    'vue/singleline-html-element-content-newline': 'off',
+    'no-tabs': 'off',
+
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
 }
--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <img
+      id="test"
+      width="125"
+      height="125"
+      class="logo"
+      alt="Vue logo"
+      src="@/assets/logo.svg"
+    />
+    <!-- test -->
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld id="11" title="xxx" msg="You did it!" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"> Home </RouterLink>
+        <RouterLink to="/about"> About </RouterLink>
       </nav>
     </div>
   </header>
